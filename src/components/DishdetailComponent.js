5cebce2af703784978e1319a9e3694c33c1dafbc
Anastasiia Rylovnikova <a.rylovnikova@gmail.com>
2020-04-13 13:38:04 +0300

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,14 +5,14 @@ import { Card, CardImg, CardText, CardBody, CardTitle,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 
 function RenderDish({dish}){
     if( dish != null) {
         return (
             <Card>
-                <CardImg width = "100%" src={dish.image} alt = {dish.name}></CardImg>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
